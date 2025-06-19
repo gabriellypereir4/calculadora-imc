c2cee5443cdd46b45829f1c754654818c34c7ad3
Gabrielly <gabriellydiaspereira15@gmail.com>
2025-06-19 15:27:40 -0300

    adicionando máscaras

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,53 @@
-const form = document.getElementById('form');
-form.addEventListener('submit', function(event) {
-    /* Previne o comportamento padrão do evento submit do JS, ou seja,
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  /* Previne o comportamento padrão do evento submit do JS, ou seja,
     impede o recarregamento da página */
-    event.preventDefault();
+  event.preventDefault();
 
-    const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
+  const weight = document.getElementById("weight").value;
+  const height = document.getElementById("height").value.replace(',' , '.')
+  
+  const bmi = (weight / (height * height)).toFixed(2);
 
-    const bmi = (weight / (height * height)).toFixed(2);
+  if (!isNaN(bmi)) {
+    const value = document.getElementById("value");
+    let description = "";
 
-    if (!isNaN(bmi)) {
-        const value = document.getElementById('value');
-        let description = '';
+    value.classList.add("attention");
 
-        value.classList.add('attention');
-        
-        document.getElementById('infos').classList.remove('hidden');
-    
-        if (bmi < 18.5){
-            description = 'Cuidado! Você está abaixo do peso!';
-        } 
+    document.getElementById("infos").classList.remove("hidden");
 
-        else if (bmi >= 18.5 && bmi <= 25) {
-            description = "Você está no peso ideal!";
-            value.classList.remove('attention');
-            value.classList.add('normal');
-        }
-
-        else if (bmi > 25 && bmi <= 30) {
-            description = "Cuidado! Você está com sobrepeso!";
-        }
-
-        else if (bmi > 30 && bmi <= 35) {
-            description = "Cuidado! Você está com obesidade moderada!";
-        }
-
-        else if (bmi > 35 && bmi <= 40) {
-            description = "Cuidado! Você está com obesidade severa!";
-        }
-
-        else {
-            description = "Cuidado! Você está com obesidade morbida!";
-        }
-
-        value.textContent = bmi.replace('.', ',');
-        document.getElementById('description').textContent = description;
+    if (bmi < 18.5) {
+      description = "Cuidado! Você está abaixo do peso!";
+    } else if (bmi >= 18.5 && bmi <= 25) {
+      description = "Você está no peso ideal!";
+      value.classList.remove("attention");
+      value.classList.add("normal");
+    } else if (bmi > 25 && bmi <= 30) {
+      description = "Cuidado! Você está com sobrepeso!";
+    } else if (bmi > 30 && bmi <= 35) {
+      description = "Cuidado! Você está com obesidade moderada!";
+    } else if (bmi > 35 && bmi <= 40) {
+      description = "Cuidado! Você está com obesidade severa!";
+    } else {
+      description = "Cuidado! Você está com obesidade morbida!";
     }
+
+    value.textContent = bmi.replace(".", ",");
+    document.getElementById("description").textContent = description;
+  }
 });
 
-form.addEventListener('keypress', function(event) {
-    if (event.key === 'press') {
-        document.getElementById('#calculate').click();
-    }
+form.addEventListener("keypress", function (event) {
+  if (event.key === "press") {
+    document.getElementById("#calculate").click();
+  }
 });
+
+function criaMascara(inputId) {
+  let valorInput = document.getElementById(inputId).value;
+  if (valorInput.length === 1) {
+    const formatado = valorInput + ",";
+    document.getElementById(inputId).value = formatado
+  }
+}
